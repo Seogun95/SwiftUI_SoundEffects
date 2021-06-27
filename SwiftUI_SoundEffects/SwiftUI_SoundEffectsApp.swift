9ba18d7136577954d1c410677f21e98afecc39c3
SeonJungKim <ksj083895@gmail.com>
2021-06-27 14:50:40 +0900

    Initial Commit

---
+++ SwiftUI_SoundEffects/SwiftUI_SoundEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SoundEffectsApp.swift
+//  SwiftUI_SoundEffects
+//
+//  Created by Seogun Kim on 2021/06/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SoundEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
